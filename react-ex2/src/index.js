d0f319abd07ac8d0c7bee0b485b63def2e053cc8
Quoc Viet <quocvietk10tt@gmail.com>
2020-04-26 16:35:01 +0700

    create react -done-

---
+++ react-ex2/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Tạo react bằng webpack và babel!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
